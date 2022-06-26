c58e1f606a63ccb3b339f2371540709c9a234234
alexandruakkol <alexandru.akkol@gmail.com>
2022-06-26 13:47:29 +0300

    revenue only

--- scraper.js
+++ scraper.js
@@ -6,29 +6,33 @@ async function puppetPageInit() {
   return page;
 }
 
-
 let symbol = "AAPL";
 
-async function scrapeSymbol(symbol){
-    const page = await puppetPageInit();
-    scrapeIncomeStatement(symbol, page);    
+async function scrapeSymbol(symbol) {
+  const page = await puppetPageInit();
+  scrapeIncomeStatement(symbol, page);
 }
 
 async function scrapeIncomeStatement(symbol, page) {
   let url = `https://roic.ai/financials/${symbol}`;
-    
-    
+
   try {
     await page.goto(url, { waitUntil: "networkidle0" });
     const data = await page.evaluate(() => {
-        let results=[];
-        let obj={};
-        const root = document.querySelector("#__next > div > main > div.w-full.mt-5.sm\\:px-5.md\\:px-20.lg\\:px-30.xl\\:px-30 > div > div > div > div.flex-col.overflow-x-auto");
-        root.childNodes.forEach(financial => {
-            obj[financial.childNodes[0].childNodes[0].innerHTML]=4
-            results.push(obj)
-        })
-        return results;
+      let results = [];
+      let obj = {};
+      let varname;
+      const root = document.querySelector(
+        "#__next > div > main > div.w-full.mt-5.sm\\:px-5.md\\:px-20.lg\\:px-30.xl\\:px-30 > div > div > div > div.flex-col.overflow-x-auto"
+      );
+      root.childNodes.forEach((financial) => {
+        varname = financial.childNodes[0].childNodes[0].innerHTML;
+        if (varname === "Revenue") {
+          obj[varname] = financial.childNodes[2].childNodes[financial.childNodes[2].childNodes.length-1].textContent;
+          results.push(obj);
+        }
+      });
+      return results;
     });
     console.log(data);
   } catch (error) {
@@ -37,4 +41,4 @@ async function scrapeIncomeStatement(symbol, page) {
 }
 
 scrapeSymbol(symbol);
-module.exports =  scrapeIncomeStatement;
+module.exports = scrapeIncomeStatement;
