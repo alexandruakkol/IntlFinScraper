4df091d5423be2a1cfbe45539da640faaa5429a1
alexandruakkol <alexandru.akkol@gmail.com>
2022-06-26 13:31:00 +0300

    init categories

---
+++ scraper.js
@@ -0,0 +1,40 @@
+const puppeteer = require("puppeteer");
+
+async function puppetPageInit() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  return page;
+}
+
+
+let symbol = "AAPL";
+
+async function scrapeSymbol(symbol){
+    const page = await puppetPageInit();
+    scrapeIncomeStatement(symbol, page);    
+}
+
+async function scrapeIncomeStatement(symbol, page) {
+  let url = `https://roic.ai/financials/${symbol}`;
+    
+    
+  try {
+    await page.goto(url, { waitUntil: "networkidle0" });
+    const data = await page.evaluate(() => {
+        let row=[];
+        let result=[];
+        const root = document.querySelector("#__next > div > main > div.w-full.mt-5.sm\\:px-5.md\\:px-20.lg\\:px-30.xl\\:px-30 > div > div > div > div.flex-col.overflow-x-auto");
+        root.childNodes.forEach(financial => {
+            row.push(financial.childNodes[0].childNodes[0].innerHTML)
+            result = row
+        })
+        return result;
+    });
+    console.log(data);
+  } catch (error) {
+    console.error(symbol, "incomeStatement", error);
+  }
+}
+
+scrapeSymbol(symbol);
+module.exports =  scrapeIncomeStatement;
