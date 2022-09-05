5d58e2c9956da0d3d269fba5efc717e1bce23225
alexandruakkol <alexandru.akkol@gmail.com>
2022-09-05 23:15:44 +0300

    wsj working

--- latest.js
+++ latest.js
@@ -1,28 +1,21 @@
-const { builtinModules } = require("module");
 const puppeteer = require("puppeteer");
 
 async function scrapeLatest(symbol, page) {
-  let url = `https://seekingalpha.com/symbol/${symbol}/balance-sheet`;
+  let url = `https://finance.yahoo.com/quote/${symbol}/financials?p=${symbol}`;
 
   try {
-    await page.goto(url, { waitUntil: "networkidle0" });
+    await page.goto(url, { waitUntil: "networkidle0"});
     const data = await page.evaluate(() => {
       let obj = {};
       let varname;
       const root = document.querySelector(
-        "#content > div > div.gB.bgA.bgR.bgU > div > div > div:nth-child(3) > div > div > section > div > div.gqM > div:nth-child(2) > div.jaA.jaF.bugA.bhGN > div > table > tbody"
+        "#Col1-1-Financials-Proxy > section > div.Pos\\(r\\) > div.W\\(100\\%\\).Whs\\(nw\\).Ovx\\(a\\).BdT.Bdtc\\(\\$seperatorColor\\) > div > div.D\\(tbrg\\)"
       );
-      console.log(root.textContent);
-      root.childNodes.forEach((financial) => {
-        console.log(financial.textContent);
-        if (!financial.childNodes[0].innerHTML) return;
-        varname = financial.childNodes[0].innerHTML;
-        obj[varname] =
-          financial.childNodes[financial.childNodes.length - 1].textContent;
-      });
-      console.log(obj);
-      return obj;
+      
+      return root.textContent;
     });
+    
+    console.log({ _id: symbol, ...data });
     return { _id: symbol, ...data };
   } catch (error) {
     console.error(symbol, "incomeStatement", error);
--- latestWsj.js
+++ latestWsj.js
@@ -1,25 +1,36 @@
-
+const puppeteer = require("puppeteer");
 
 async function scrapeLatest(symbol, page) {
     let url = `https://www.wsj.com/market-data/quotes/${symbol}/financials/quarter/balance-sheet`;
 
     try {
-      await page.goto(url, { waitUntil: "networkidle0" });
+      await page.goto(url, { waitUntil: "networkidle0"});
       const data = await page.evaluate(() => {
-        let obj = [];
+        let obj = {}
         const root = document.querySelector("#cr_cashflow > div.expanded > div.cr_cashflow_table > table > tbody");
-        
-        root.childNodes.forEach((financial)=> {
-          if(financial.childElementCount && !financial.className) obj.financial.childNodes[1].textContent=financial.childNodes[financial.childNodes.length-1].textContent
-        })
 
+        Array.prototype.forEach.call(root.childNodes, (financial)=>{
+          if(financial.className!='hide' && financial.nodeName!='#text'){
+            let key = financial.childNodes[1].textContent;
+            obj[key]= financial.childNodes[3].textContent}
+        })
+        
+        console.log(obj);
         return obj;
       })
       return data;
     } catch (error) {
-      console.error(symbol, "incomeStatement", error);
+      console.log(symbol, "incomeStatement", error);
     }
   }
 
-module.exports=scrapeLatest;
+  //TESTING
+(async () => {
+  const browser = await puppeteer.launch({ headless: false,});
+  const page = await browser.newPage();
+  return { page, browser };
+})().then(async (init) => {
+  scrapeLatest("AAPL", init.page);
+});
+
 
