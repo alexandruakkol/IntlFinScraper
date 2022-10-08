7689d0c83cc20e8cc26441d93ceb94e5fd835cf8
alexxme211 <92181364+alexxme211@users.noreply.github.com>
2022-10-08 21:14:35 +0300

    cloud db integr

--- latestWsj.js
+++ latestWsj.js
@@ -1,44 +1,41 @@
-const puppeteer = require("puppeteer");
-const compute = require("./compute");
-const insertRow = require("./postgres");
+const puppeteer = require("puppeteer"),
+  compute = require("./compute"),
+  insertRow = require("./postgres"),
+  getTickers = require("./getAllUSTickers");
 
-const dbData ={
-  'symbol':'symbol',
-  'date':'date',
-  'Total Current Assets':'curassets',
-  'Net Property, Plant & Equipment':'neppropertyplants',
-  'Intangible Assets':'intangibles',
-  'Total Assets':'totalassets',
-  'Price':'price',
-  'Total Current Liabilities':'currentliabilities',
-  'Quick Ratio':'quickratio',
-  'Long-Term Debt':'ltdebt',
-  'Total Liabilities':'totalliabilities',
-  "Total Shareholders' Equity":'totalshareholdequity',
-  'Total Equity':'totalequity',
-  'Cost of Goods Sold (COGS) incl. D&A':'cogsplusda',
-  'Depreciation & Amortization Expense':'depramortiz',
-  'Research & Development':'rdexpenses',
-  'Net Income':'netincome',
-  'EPS (Basic)':'eps',
-  'Basic Shares Outstanding':'sharesoutst',
-  'EBITDA':'ebitda',
-  'EBITDA Margin':'ebitdamg',
-  'Cash & Short Term Investments':'cashandst',
-  'mcap':'mcap',
-  'ncavpsppct':'ncavpsppct',
-  'ncavpsppct_fixed':'ncavpsppct_fixed',
-  'roepct':'roepct',
-  'roce':'roce',
-  'pe':'pe',
-  'depct':'depct',
-  'interestRatepct':'interestratepct'
-}
- 
+const dbData = {
+  symbol: "symbol",
+  date: "date",
+  "Total Current Assets": "curassets",
+  "Net Property, Plant & Equipment": "neppropertyplants",
+  "Intangible Assets": "intangibles",
+  "Total Assets": "totalassets",
+  Price: "price",
+  "Total Current Liabilities": "currentliabilities",
+  "Quick Ratio": "quickratio",
+  "Long-Term Debt": "ltdebt",
+  "Total Liabilities": "totalliabilities",
+  "Total Shareholders' Equity": "totalshareholdequity",
+  "Total Equity": "totalequity",
+  "Cost of Goods Sold (COGS) incl. D&A": "cogsplusda",
+  "Depreciation & Amortization Expense": "depramortiz",
+  "Research & Development": "rdexpenses",
+  "Net Income": "netincome",
+  "EPS (Basic)": "eps",
+  "Basic Shares Outstanding": "sharesoutst",
+  EBITDA: "ebitda",
+  "EBITDA Margin": "ebitdamg",
+  "Cash & Short Term Investments": "cashandst",
+  mcap: "mcap",
+  ncavpsppct: "ncavpsppct",
+  ncavpsppct_fixed: "ncavpsppct_fixed",
+  roepct: "roepct",
+  roce: "roce",
+  pe: "pe",
+  depct: "depct",
+  interestRatepct: "interestratepct",
+};
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
 async function scrapeLatest(symbol, page) {
   let balanceSheetURL = `https://www.wsj.com/market-data/quotes/${symbol}/financials/quarter/balance-sheet`;
   let incomeStatementURL = `https://www.wsj.com/market-data/quotes/${symbol}/financials/quarter/income-statement`;
@@ -51,7 +48,7 @@ async function scrapeLatest(symbol, page) {
       { waitUntil: "domcontentloaded" },
       { timeout: 0 }
     );
-    sleep(1000);
+
     const balanceSheet = await page.evaluate(() => {
       const error = document.querySelector("#cr_cashflow > span");
       if (error) return { error: "pageDown" };
@@ -102,7 +99,6 @@ async function scrapeLatest(symbol, page) {
       { waitUntil: "domcontentloaded" },
       { timeout: 0 }
     );
-    sleep(1000);
     const incomeStatement = await page.evaluate(() => {
       let obj = {};
 
@@ -144,28 +140,34 @@ async function makeBrowser() {
 }
 
 makeBrowser().then(async (init) => {
-  const symbol = "ebay";
-
-  let tryCounter = 1;
-  while (tryCounter < 6) {
-    resp = await scrapeLatest(symbol, init.page);
-    if (resp != "error") {
-      try {
-        resp = compute(resp);
-        resp={...resp,...{symbol}}
-        Object.entries(dbData).forEach(pair=>{ //translate keys via dbData dictionary
-          let key=pair[0], newkey=dbData[key];
-          resp[newkey]=resp[key];
-        })
-        console.log('resp',resp)
-        insertRow(Object.entries(resp).filter(pair=>Object.values(dbData).includes(pair[0])));
-      } catch (er) {
-        console.log(`${symbol} computing error, ${er}`);
+  const tickers = await getTickers();
+  for (symbol of tickers) {
+    let tryCounter = 1;
+    while (tryCounter < 6) {
+      resp = await scrapeLatest(symbol, init.page);
+      if (resp != "error") {
+        try {
+          resp = compute(resp);
+          resp = { ...resp, ...{ symbol } };
+          Object.entries(dbData).forEach((pair) => {
+            //translate keys via dbData dictionary
+            let key = pair[0],
+              newkey = dbData[key];
+            resp[newkey] = resp[key];
+          });
+          insertRow(
+            Object.entries(resp).filter((pair) =>
+              Object.values(dbData).includes(pair[0])
+            )
+          );
+        } catch (er) {
+          console.log(`${symbol} computing error, ${er}`);
+        }
+        break;
       }
-      break;
+      tryCounter++;
+      console.log(`${symbol} scraping fail: try #${tryCounter}`);
     }
-    tryCounter++;
-    console.log(`${symbol} scraping fail: try #${tryCounter}`);
   }
   init.browser.close();
 });
--- postgres.js
+++ postgres.js
@@ -1,25 +1,33 @@
-const { Client } = require('pg')
-require('dotenv').config();
-const {user, host, database, password, port} = process.env
+const { Client } = require("pg");
+require("dotenv").config();
+const { user, host, database, password, port } = process.env;
 const client = new Client({
-  user
-  ,host
-  ,database
-  ,password
-  ,port
-}) 
-client.connect()
+  user,
+  host,
+  database,
+  password,
+  port,
+});
+client.connect();
+console.log("DB client open");
 
-function insertRow(data){
-  console.log('data',data)
-  const keys = JSON.stringify(data.map(pair=>pair[0])).replace('[','').replace(']','').replace(/['"]+/g, '');
-  const values = JSON.stringify(data.map(pair=>isNaN(pair[1])?pair[1]:Number(pair[1]))).replace('[','').replace(']','').replaceAll(`"`,`'`);
-  console.log(`INSERT INTO data(${keys}) values(${values})`);
+function insertRow(data) {
+  const keys = JSON.stringify(data.map((pair) => pair[0]))
+    .replace("[", "")
+    .replace("]", "")
+    .replace(/['"]+/g, "");
+  const values = JSON.stringify(
+    data.map((pair) => (isNaN(pair[1]) ? pair[1] : Number(pair[1])))
+  )
+    .replace("[", "")
+    .replace("]", "")
+    .replaceAll(`"`, `'`);
+  //console.log(`INSERT INTO data(${keys}) values(${values})`);
   client.query(`INSERT INTO data(${keys}) values(${values})`, (err, res) => {
-    console.log(res);
-    console.log('db insert error ', err)
-    client.end()
-   })
+    let symbol = data.filter((p) => p[0] == "symbol");
+    console.log(symbol[0][1], "written to DB");
+    if (err) console.log("db insert error ", err);
+  });
 }
 
 module.exports = insertRow;
